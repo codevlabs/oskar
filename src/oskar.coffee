591b6eac04518ce357b457772a561d4d937cdfcc
Marcel <marcel@hanno.co>
2015-06-02 23:11:41 +0200

    add new files after name change

---
+++ src/oskar.coffee
@@ -0,0 +1,196 @@
+express = require 'express'
+MongoClient = require './modules/mongoClient'
+SlackClient = require './modules/slackClient'
+TimeHelper = require './helper/timeHelper'
+InputHelper = require './helper/inputHelper'
+
+class Oskar
+
+  constructor: (mongo, slack) ->
+    @app = express()
+    @app.set 'view engine', 'ejs'
+    @app.set 'views', 'src/views/'
+    @app.use '/public', express.static(__dirname + '/public')
+
+    @mongo = mongo || new MongoClient()
+    @mongo.connect()
+
+    @slack = slack || new SlackClient()
+    @slack.connect()
+
+    @setupEvents()
+    @setupRoutes()
+
+    # check for user's status every hour
+    setInterval ->
+      @checkForUserStatus (@slack)
+    , 3600 * 1000
+
+  setupEvents: () ->
+    @slack.on 'presence', @presenceHandler
+    @slack.on 'message', @messageHandler
+
+  setupRoutes: () ->
+    @app.set 'port', process.env.PORT || 5000
+
+    @app.get '/', (req, res) =>
+      users = @slack.getUsers()
+      userIds = users.map (user) ->
+        return user.id
+      @mongo.getAllUserFeedback(userIds).then (statuses) =>
+        filteredStatuses = []
+        statuses.forEach (status) ->
+          filteredStatuses[status.id] = status.feedback
+        res.render('pages/index', { users: users, statuses: filteredStatuses })
+
+    @app.listen @app.get('port'), ->
+      console.log "Node app is running on port 5000"
+
+  presenceHandler: (data) =>
+
+    @mongo.userExists(data.userId).then (res) =>
+      if !res
+        user = @slack.getUser(data.userId)
+        @mongo.saveUser(user).then (res) =>
+          # @presenceHandler data
+      @requestUserFeedback data.userId, data.status
+
+  messageHandler: (message) =>
+
+    # if user is asking for feedback of user with ID
+    if userId = InputHelper.isAskingForUserStatus(message.text)
+      return @revealStatus userId, message
+
+    # if comment is allowed, save in DB
+    if @slack.isUserCommentAllowed message.user
+      return @handleFeedbackMessage message
+
+    # check last feedback timestamp and evaluate feedback
+    @mongo.getLatestUserTimestampForProperty('feedback', message.user).then (timestamp) =>
+      @evaluateFeedback message, timestamp
+
+  requestUserFeedback: (userId, status) ->
+
+    @mongo.getLatestUserTimestampForProperty('feedback', userId).then (res) =>
+
+      # if user doesnt exist, skip
+      if res is false
+        return
+
+      @mongo.saveUserStatus userId, status
+
+      # if user switched to anything but active or triggered, skip
+      if status != 'active' && status != 'triggered'
+        return
+
+      # if it's weekend, skip
+      if TimeHelper.isWeekend()
+        return
+
+      # if current time is not in interval, skip
+      # userLocalDate = timeHelper.getLocalDate null, user.tz_offset / 3600
+      # if !timeHelper.isDateInsideInterval 8, 12, userLocalDate
+      #   return
+      #
+
+      # if last activity (res) is null or timestamp has expired, ask for status
+      if (res is null || TimeHelper.hasTimestampExpired 20, res)
+        @composeMessage userId, 'requestFeedback'
+
+  evaluateFeedback: (message, latestFeedbackTimestamp) ->
+
+    # if user has already submitted feedback in the last x hours, reject
+    if (latestFeedbackTimestamp && !TimeHelper.hasTimestampExpired 20, latestFeedbackTimestamp)
+      return @composeMessage message.user, 'alreadySubmitted'
+
+    # if user didn't send valid feedback
+    if !InputHelper.isValidStatus message.text
+      return @composeMessage message.user, 'invalidInput'
+
+    @mongo.saveUserFeedback message.user, message.text
+
+    # if feedback is lower than 5, ask user for additional feedback
+    if (parseInt(message.text) < 5)
+      @slack.allowUserComment message.user
+      return @composeMessage message.user, 'lowFeedback'
+
+    @composeMessage message.user, 'feedbackReceived'
+
+  revealStatus: (userId, message) =>
+    if userId is 'channel'
+      @revealStatusForChannel(message.user)
+    else
+      @revealStatusForUser(message.user, userId)
+
+  revealStatusForChannel: (userId) =>
+    userIds = @slack.getUserIds()
+    @mongo.getAllUserFeedback(userIds).then (res) =>
+      @composeMessage userId, 'revealChannelStatus', res
+
+  revealStatusForUser: (userId, targetUserId) =>
+    userObj = @slack.getUser targetUserId
+    @mongo.getLatestUserFeedback(targetUserId).then (res) =>
+      if (res)
+        res.user = userObj
+      @composeMessage userId, 'revealUserStatus', res
+
+  handleFeedbackMessage: (message) =>
+    @slack.disallowUserComment message.user
+    @mongo.saveUserFeedbackMessage message.user, message.text
+    @composeMessage message.user, 'feedbackMessageReceived'
+
+  composeMessage: (userId, messageType, obj) ->
+
+    # request feedback
+    if messageType is 'requestFeedback'
+      statusMsg = ''
+
+    # channel info
+    if messageType is 'revealChannelStatus'
+      obj.forEach (user) =>
+        userObj = @getUser user.id
+        statusMsg += "#{userObj.profile.first_name} is feeling *#{user.feedback.status}*"
+        if user.feedback.message
+          statusMsg += " (#{user.feedback.message})"
+        statusMsg += ".\r\n"
+
+    # user info
+    if messageType is 'revealUserStatus'
+      if !obj
+        statusMsg = "Oh, it looks like I haven\'t heard from #{obj.user.profile.first_name} for a while. Sorry!"
+      else
+        statusMsg = "#{obj.user.profile.first_name} is feeling *#{obj.status}* on a scale from 0 to 9."
+        if res.message
+          statusMsg += "\r\nThe last time I asked him what\'s up he replied: #{obj.message}"
+
+    # already submitted
+    if messageType is 'alreadySubmitted'
+      statusMsg = 'Oops, looks like I\'ve already received some feedback from you in the last 20 hours.'
+
+    # invalid input
+    if messageType is 'invalidInput'
+      statusMsg = 'Oh it looks like you want to tell me how you feel, but unfortunately I only understand numbers between 0 and 9'
+
+    # low feedback
+    if messageType is 'lowFeedback'
+      statusMsg = 'Feel free to share with me what\'s wrong. I will treat it with confidence'
+
+    # feedback already received
+    if messageType is 'feedbackReceived'
+      statusMsg = ''
+
+    # feedback received
+    if messageType is 'feedbackMessageReceived'
+      statusMsg = 'Thanks, my friend. I really appreciate your openness.'
+
+    @slack.postMessage(userId, statusMsg)
+
+  checkForUserStatus: (slack) =>
+    userIds = slack.getUserIds()
+    userIds.forEach (userId) ->
+      data =
+        userId: userId
+        status: 'triggered'
+      slack.emit 'presence', data
+
+module.exports = Oskar
---
+++ src/oskar.js
@@ -0,0 +1,233 @@
+var InputHelper, MongoClient, Oskar, SlackClient, TimeHelper, express,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+express = require('express');
+
+MongoClient = require('./modules/mongoClient');
+
+SlackClient = require('./modules/slackClient');
+
+TimeHelper = require('./helper/timeHelper');
+
+InputHelper = require('./helper/inputHelper');
+
+Oskar = (function() {
+  function Oskar(mongo, slack) {
+    this.checkForUserStatus = __bind(this.checkForUserStatus, this);
+    this.handleFeedbackMessage = __bind(this.handleFeedbackMessage, this);
+    this.revealStatusForUser = __bind(this.revealStatusForUser, this);
+    this.revealStatusForChannel = __bind(this.revealStatusForChannel, this);
+    this.revealStatus = __bind(this.revealStatus, this);
+    this.messageHandler = __bind(this.messageHandler, this);
+    this.presenceHandler = __bind(this.presenceHandler, this);
+    this.app = express();
+    this.app.set('view engine', 'ejs');
+    this.app.set('views', 'src/views/');
+    this.app.use('/public', express["static"](__dirname + '/public'));
+    this.mongo = mongo || new MongoClient();
+    this.mongo.connect();
+    this.slack = slack || new SlackClient();
+    this.slack.connect();
+    this.setupEvents();
+    this.setupRoutes();
+    setInterval(function() {
+      return this.checkForUserStatus(this.slack);
+    }, 3600 * 1000);
+  }
+
+  Oskar.prototype.setupEvents = function() {
+    this.slack.on('presence', this.presenceHandler);
+    return this.slack.on('message', this.messageHandler);
+  };
+
+  Oskar.prototype.setupRoutes = function() {
+    this.app.set('port', process.env.PORT || 5000);
+    this.app.get('/', (function(_this) {
+      return function(req, res) {
+        var userIds, users;
+        users = _this.slack.getUsers();
+        userIds = users.map(function(user) {
+          return user.id;
+        });
+        return _this.mongo.getAllUserFeedback(userIds).then(function(statuses) {
+          var filteredStatuses;
+          filteredStatuses = [];
+          statuses.forEach(function(status) {
+            return filteredStatuses[status.id] = status.feedback;
+          });
+          return res.render('pages/index', {
+            users: users,
+            statuses: filteredStatuses
+          });
+        });
+      };
+    })(this));
+    return this.app.listen(this.app.get('port'), function() {
+      return console.log("Node app is running on port 5000");
+    });
+  };
+
+  Oskar.prototype.presenceHandler = function(data) {
+    return this.mongo.userExists(data.userId).then((function(_this) {
+      return function(res) {
+        var user;
+        if (!res) {
+          user = _this.slack.getUser(data.userId);
+          _this.mongo.saveUser(user).then(function(res) {});
+        }
+        return _this.requestUserFeedback(data.userId, data.status);
+      };
+    })(this));
+  };
+
+  Oskar.prototype.messageHandler = function(message) {
+    var userId;
+    if (userId = InputHelper.isAskingForUserStatus(message.text)) {
+      return this.revealStatus(userId, message);
+    }
+    if (this.slack.isUserCommentAllowed(message.user)) {
+      return this.handleFeedbackMessage(message);
+    }
+    return this.mongo.getLatestUserTimestampForProperty('feedback', message.user).then((function(_this) {
+      return function(timestamp) {
+        return _this.evaluateFeedback(message, timestamp);
+      };
+    })(this));
+  };
+
+  Oskar.prototype.requestUserFeedback = function(userId, status) {
+    return this.mongo.getLatestUserTimestampForProperty('feedback', userId).then((function(_this) {
+      return function(res) {
+        if (res === false) {
+          return;
+        }
+        _this.mongo.saveUserStatus(userId, status);
+        if (status !== 'active' && status !== 'triggered') {
+          return;
+        }
+        if (TimeHelper.isWeekend()) {
+          return;
+        }
+        if (res === null || TimeHelper.hasTimestampExpired(20, res)) {
+          return _this.composeMessage(userId, 'requestFeedback');
+        }
+      };
+    })(this));
+  };
+
+  Oskar.prototype.evaluateFeedback = function(message, latestFeedbackTimestamp) {
+    if (latestFeedbackTimestamp && !TimeHelper.hasTimestampExpired(20, latestFeedbackTimestamp)) {
+      return this.composeMessage(message.user, 'alreadySubmitted');
+    }
+    if (!InputHelper.isValidStatus(message.text)) {
+      return this.composeMessage(message.user, 'invalidInput');
+    }
+    this.mongo.saveUserFeedback(message.user, message.text);
+    if (parseInt(message.text) < 5) {
+      this.slack.allowUserComment(message.user);
+      return this.composeMessage(message.user, 'lowFeedback');
+    }
+    return this.composeMessage(message.user, 'feedbackReceived');
+  };
+
+  Oskar.prototype.revealStatus = function(userId, message) {
+    if (userId === 'channel') {
+      return this.revealStatusForChannel(message.user);
+    } else {
+      return this.revealStatusForUser(message.user, userId);
+    }
+  };
+
+  Oskar.prototype.revealStatusForChannel = function(userId) {
+    var userIds;
+    userIds = this.slack.getUserIds();
+    return this.mongo.getAllUserFeedback(userIds).then((function(_this) {
+      return function(res) {
+        return _this.composeMessage(userId, 'revealChannelStatus', res);
+      };
+    })(this));
+  };
+
+  Oskar.prototype.revealStatusForUser = function(userId, targetUserId) {
+    var userObj;
+    userObj = this.slack.getUser(targetUserId);
+    return this.mongo.getLatestUserFeedback(targetUserId).then((function(_this) {
+      return function(res) {
+        if (res) {
+          res.user = userObj;
+        }
+        return _this.composeMessage(userId, 'revealUserStatus', res);
+      };
+    })(this));
+  };
+
+  Oskar.prototype.handleFeedbackMessage = function(message) {
+    this.slack.disallowUserComment(message.user);
+    this.mongo.saveUserFeedbackMessage(message.user, message.text);
+    return this.composeMessage(message.user, 'feedbackMessageReceived');
+  };
+
+  Oskar.prototype.composeMessage = function(userId, messageType, obj) {
+    var statusMsg;
+    if (messageType === 'requestFeedback') {
+      statusMsg = '';
+    }
+    if (messageType === 'revealChannelStatus') {
+      obj.forEach((function(_this) {
+        return function(user) {
+          var userObj;
+          userObj = _this.getUser(user.id);
+          statusMsg += "" + userObj.profile.first_name + " is feeling *" + user.feedback.status + "*";
+          if (user.feedback.message) {
+            statusMsg += " (" + user.feedback.message + ")";
+          }
+          return statusMsg += ".\r\n";
+        };
+      })(this));
+    }
+    if (messageType === 'revealUserStatus') {
+      if (!obj) {
+        statusMsg = "Oh, it looks like I haven\'t heard from " + obj.user.profile.first_name + " for a while. Sorry!";
+      } else {
+        statusMsg = "" + obj.user.profile.first_name + " is feeling *" + obj.status + "* on a scale from 0 to 9.";
+        if (res.message) {
+          statusMsg += "\r\nThe last time I asked him what\'s up he replied: " + obj.message;
+        }
+      }
+    }
+    if (messageType === 'alreadySubmitted') {
+      statusMsg = 'Oops, looks like I\'ve already received some feedback from you in the last 20 hours.';
+    }
+    if (messageType === 'invalidInput') {
+      statusMsg = 'Oh it looks like you want to tell me how you feel, but unfortunately I only understand numbers between 0 and 9';
+    }
+    if (messageType === 'lowFeedback') {
+      statusMsg = 'Feel free to share with me what\'s wrong. I will treat it with confidence';
+    }
+    if (messageType === 'feedbackReceived') {
+      statusMsg = '';
+    }
+    if (messageType === 'feedbackMessageReceived') {
+      statusMsg = 'Thanks, my friend. I really appreciate your openness.';
+    }
+    return this.slack.postMessage(userId, statusMsg);
+  };
+
+  Oskar.prototype.checkForUserStatus = function(slack) {
+    var userIds;
+    userIds = slack.getUserIds();
+    return userIds.forEach(function(userId) {
+      var data;
+      data = {
+        userId: userId,
+        status: 'triggered'
+      };
+      return slack.emit('presence', data);
+    });
+  };
+
+  return Oskar;
+
+})();
+
+module.exports = Oskar;
