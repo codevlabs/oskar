6a71f36d0a53c43201efa2ae1936e8bd4c3b2da0
Jon Lay <jon@hanno.co>
2015-07-10 10:12:03 +0800

    Finish rewriting most of the message strings

--- src/content/oskarTexts.js
+++ src/content/oskarTexts.js
@@ -20,33 +20,33 @@ OskarTexts = {
     '4': 'really good',
     '5': 'awesome'
   },
-  introduction: "Hey there {0}, let me quickly introduce myself.\nMy name is Oskar, I'm your new happiness coach on Slack. I'm not going to bother you a lot, but every once in a while, I'm gonna ask how you feel, ok?\nLet me know when you're ready! Ah, and if you want to know a little bit more about me and what I do, check out the <http://oskar.herokuapp.com/faq|Oskar FAQ> or simply send me a message with 'help' and I'll send you that link again.",
-  firstMessage: "Cool! I'm ready as well. From now on I'll ask you this simple question: 'How is it going?'\nYou can reply to it with a number between 1 and 5. OK? Let's give it a try, type a number between 1 and 5",
-  firstMessageSuccess: "That was easy, wasn't it? Let me now tell you what each of these numbers mean:\n5) Awesome :heart_eyes_cat:\n4) Really good :smile:\n3) Alright :neutral_face:\n2) A bit down :pensive:\n1) Pretty bad :tired_face:\n'That's it for now. Next time I'm gonna ask you will be tomorrow when you're back online. Have a great day and see you soon!",
-  firstMessageFailure: "Oh it looks like you want to tell me how you feel, but unfortunately I only understand numbers between 1 and 5",
+  introduction: "Hey {0}! Let me quickly introduce myself.\nMy name is Oskar, and the team has drafted me in to be their new happiness coach on Slack. I'm not going to bother you a lot, but every once in a while (usually twice a day), I'm gonna ask how you feel. Ready? *Just reply to this message and we'll give is a try* :smile:",
+  firstMessage: "Alright! From now on, I'll check in once in a while to ask how you're feeling?'\n\nYou can reply to me with a number between 1 and 5, and I'll keep track of your answers over time and share them with your team.\n\nOK? Let's give it a try: *How do you feel right now?*\n (5) :heart_eyes_cat: Awesome\n (4) :smile: Really good\n (3) :neutral_face: Alright\n (2) :pensive: A bit down\n (1) :tired_face: Pretty bad\n",
+  firstMessageSuccess: "That was easy, wasn't it? :smile: I'm gonna disappear for a few hours now, but I'll check in on you in a couple of hours, or tomorrow, if I miss you.",
+  firstMessageFailure: "Whoops, it looks like you're trying to tell me how you feel, but unfortunately I only understand numbers between 1 and 5. Can you give it another go?",
   requestFeedback: {
-    random: ["Hey {0}, How's everything going? Hit me with a number from 1-5, and I'll share it with the rest of the team.\n", "Hello, me again! Just checking in to see how you're feeling. Want to share?\n", "Nice to see you, {0}, Wanna tell me how you feel? A number between 1 and 5 is enough. \n"],
+    random: ["Hey {0}, How are you feeling right now? Hit me with a number and I'll share it with the rest of our team.\n", "Hello, me again! Just checking in to see how you're feeling. Want to share?\n", "Nice to see you, {0}, Wanna tell me how you feel? A number between 1 and 5 is all I need.\n"],
     selection: "(5) :heart_eyes_cat: Awesome\n (4) :smile: Really good\n (3) :neutral_face: Alright\n (2) :pensive: A bit down\n (1) :tired_face: Pretty bad\n",
-    options: ["Hey, didn't you see my last message? It'll only take a second of your time to tell me how you're doing :wink:", "Hellooooo, what's up? Do you want to ignore me? Just give me a number between 1 and 5 and I'll not bother you any longer."]
+    options: ["Perhaps you missed my last message... I'd really love to hear how you're doing. Would you mind letting me know?", "Hey, looks like you missed me last time, but if you can give me a number between 1-5 to let me know how you feel, I'll be on my way :smile:"]
   },
-  faq: "Looks like you need some help. Here's the link to the <http://oskar.herokuapp.com/faq|Oskar FAQ>",
+  faq: "Looks like you need a little help. Here's the link to the <http://oskar.herokuapp.com/faq|Oskar FAQ>",
   revealChannelStatus: {
-    status: "*{0}* feels *{1}/5*",
+    status: "*{0}* is feeling *{1}/5*",
     message: "\n>\"{0}\""
   },
   revealUserStatus: {
     error: "Oh, it looks like I haven\'t heard from {0} for a while. Sorry!",
-    status: "{0} is feeling *{1}/5* at the moment.",
+    status: "*{0}* is feeling *{1}/5*",
     message: "\n>\"{0}\""
   },
   newUserFeedback: "*{0}* is feeling *{1}/5*\n>\"{2}\"",
   alreadySubmitted: ["Oops, looks like you've already told me how you feel in the last couple of hours. Let's check in again later.", "Oh, hey there! I actually have some feedback from you already, in the last 4 hours. Let's leave it a little longer before we catch up :smile:", "Easy, tiger! I know you love our number games, but let\'s wait a bit before we play again! I'll ping you in a few hours to see how you're doing."],
   invalidInput: ["Oh! I'm not sure what you meant, there: I only understand numbers between 1 and 5. Do you mind phrasing that a little differently?", "Oh! I'm not sure what you meant, there: I only understand numbers between 1 and 5. Do you mind phrasing that a little differently?", "I\'d really love to understand what you\'re saying, but until I become a little more educated, I'll need you to stick to using numbers between 1 and 5 to tell me how you feel."],
-  lowFeedback: ["That sucks. I was really hoping you'd be feeling a little better than that. Is there anything I should know?", "Oh dear :worried: I\'m having a rough day over here too... I think I didn't get enough sleep last night. Wanna tell me a little more? Perhaps one of your teammates will be able to help out.", "No worries, my friend! You will already feel a bit better when you tell me what\'s on your mind?"],
-  averageFeedback: ["OK. Go get em :tiger: ..if you've got something you want to share feel free. If not have a grrreat day!", "OK. Go get em :tiger: ..if you've got something you want to share feel free. If not have a grrreat day!", "OK. Go get em :tiger: ..if you've got something you want to share feel free. If not have a grrreat day!"],
-  highFeedback: [":trophy: Winning! It\'s so great to hear that. Wanna tell me why things are going so well?", ":thumbsup: looks like you\'re on :fire: today. Is there anything you\'d like to share with the team?", "There's nothing I like more than great feedback! :clap::skin-tone-4: What\'s made you feel so awesome today?"],
+  lowFeedback: ["That sucks. I was really hoping you'd be feeling a little better than that. *Is there anything I should know?*", "Oh dear :worried: I'm having a rough day over here too... :tired_face: *Wanna tell me a little more?* Perhaps one of our teammates will be able to help out.", "Ah, man :disappointed: I really hate to hear when a friend is having a rough time. *Wanna explain a little more?* "],
+  averageFeedback: ["Alright! Go get em :tiger: If you've got something you want to share, feel free. If not, have a grrreat day!", "Good luck with powering through things. if you've got something you want to share, feel free.", "That's alright. We can't all be having crazy highs and killer lows. Sometimes a little bit of normality is If you feel like telling me more, feel free!"],
+  highFeedback: [":trophy: Winning! It's so great to hear that. Wanna tell me why things are going so well?", ":thumbsup: looks like you're on :fire: today. Is there anything you'd like to share with the team?", "There's nothing I like more than great feedback! :clap::skin-tone-4: What's made you feel so awesome today?"],
   feedbackReceived: ["Thanks a lot, buddy! Keep up the good work!", "You\'re a champion. Thanks for the input and have a great day!", "That\'s really helpful. I wish you good luck with everything today!"],
-  feedbackMessageReceived: ["Thanks, my friend. I really appreciate your openness.", "That\'s the kind of feedback I wanted to hear. Much appreciated", "That\'s really helpful. I wish you good luck with everything today!"]
+  feedbackMessageReceived: ["Thanks for sharing with me, my friend. Let's catch up again soon.", "Thanks for being so open! Let's catch up in a few hours.", "Gotcha. I've pinged that message to the rest of the team."]
 };
 
 module.exports = OskarTexts;
